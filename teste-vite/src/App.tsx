c322750b4b0f01ec3e2ead2f704469529dd81ac0
CASilva <casilva.com@gmail.com>
2023-11-20 20:53:45 -0300

    Notification center

--- teste-vite/src/App.tsx
+++ teste-vite/src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { NotificationCenter } from "./NotificationCenter";
+import { ToastContainer, toast, TypeOptions } from "react-toastify";
+import "react-toastify/ReactToastify.min.css";
+
+const types = ["success", "info", "warning", "error"];
+
+export default function App() {
+  const addNotification = () => {
+    // use a random type of notification
+    toast("Aqui será escrita alguma mensagem...", {
+      type: types[Math.floor(Math.random() * types.length)] as TypeOptions
+    });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <button onClick={addNotification}>Notificar</button>
+      <hr />
+      <NotificationCenter />
+      <ToastContainer position="top-right" newestOnTop />
+    </div>
+  );
 }
-
-export default App
